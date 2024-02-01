c71cc2ff42a0ca095a6d0bfe08547536a8737dec
Dominik Kowalik <domi.kowalik@gmail.com>
2024-02-01 14:48:32 +0100

    Fix issues in User collection

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -26,7 +26,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:55',
-            'email' => 'required|email|unique:users,email,' . $this->id,
+            'email' => 'required|email|unique:users,email',
             'password' => [
                 'required',
                 'confirmed',
